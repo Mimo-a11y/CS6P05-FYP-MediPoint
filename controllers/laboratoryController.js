dcdca8a46c932b239c546a37edccc8735be610ce
Mimona Karki <kmimo7na@gmail.com>
2022-03-28 13:29:19 +0545

    Email templates added

--- controllers/laboratoryController.js
+++ controllers/laboratoryController.js
@@ -121,6 +121,7 @@ const uploadReports = async (req,res) => {
                         {where: {Report_ID: req.params.reportid, Test_No: req.params.testno}}
                     ).catch((err) => {console.log(err)});
 
+                    //FOR EMAIL SENDING
                     const patient = await Patient.findOne({
                         attributes:['P_ID'],
                         where: {P_ID: req.params.pid},
@@ -135,18 +136,138 @@ const uploadReports = async (req,res) => {
                     });
                      // e-mail message options
   let mailOptions = {
-    from: 'kmimo7na@gmail.com',
+    from: {
+        name: 'MediPoint',
+        address: 'medipoint72@gmail.com'
+      },
     to: patient.User.Email,
     subject: 'Lab test completed',
-    text: `Hello ${patient.User.Full_Name}, the reports for ${labTest.Test_Name} has been published. Please login into your account to view your reports.`
+    html: ` 
+    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed;background-color:#f9f9f9" id="bodyTable">
+        <tbody>
+            <tr>
+                <td style="padding-right:10px;padding-left:10px;" align="center" valign="top" id="bodyCell">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperWebview" style="max-width:600px">
+                        <tbody>
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                            <tr>
+                                                <td style="padding-top: 20px; padding-bottom: 20px; padding-right: 0px;" align="right" valign="middle" class="webview"> <a href="#" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:right;text-decoration:underline;padding:0;margin:0" target="_blank" class="text hideOnMobile"></a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperBody" style="max-width:600px">
+                        <tbody>
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableCard" style="background-color:#fff;border-color:#e5e5e5;border-style:solid;border-width:0 1px 1px 1px;">
+                                        <tbody>
+                                            <tr>
+                                                <td style="background-color:#00d2f4;font-size:1px;line-height:3px" class="topBorder" height="3">&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-top: 60px; padding-bottom: 20px;" align="center" valign="middle" class="emailLogo">
+                                                    <a href="#" style="text-decoration:none" target="_blank">
+                                                        <img alt="" border="0" src="https://i.ibb.co/6mRFFzS/logo.png" style="width:100%;max-width:150px;height:auto;display:block" width="150">
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
+                                                    <h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Hi, ${patient.User.Full_Name}</h2>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-bottom: 30px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="subTitle">
+                                                    <h4 class="text" style="color:#999;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:16px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:24px;text-transform:none;text-align:center;padding:0;margin:0">This a notification for your medical lab tests at Tarakeshwor Health Clinic</h4>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left:20px;padding-right:20px" align="center" valign="top" class="containtTable ui-sortable">
+                                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableDescription" style="">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="padding-bottom: 20px;" align="center" valign="top" class="description">
+                                                                    <p class="text" style="color:#666;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:14px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:22px;text-transform:none;text-align:center;padding:0;margin:0">Your <b>${labTest.Test_Name}</b> has been completed. The lab reports are uploaded in your MediPoint app. Kindly, login to the app to view your reports.</p>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-size:1px;line-height:1px" height="20">&nbsp;</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="space">
+                                        <tbody>
+                                            <tr>
+                                                <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperFooter" style="max-width:600px">
+                        <tbody>
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" class="footer">
+                                        <tbody>
+                                            <tr>
+                                                <td style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px" align="center" valign="top" class="socialLinks">
+                                                    <a href="#facebook-link" style="display:inline-block" target="_blank" class="facebook">
+                                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/facebook.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                                    </a>
+                                                    <a href="#instagram-link" style="display: inline-block;" target="_blank" class="instagram">
+                                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/instagram.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding: 10px 10px 5px;" align="center" valign="top" class="brandInfo">
+                                                    <p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">©&nbsp;MediPoint Inc. | Tarakeshwor, 08 | Kathmandu, NEPAL.</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding: 0px 10px 10px;" align="center" valign="top" class="footerEmailInfo">
+                                                    <p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">If you have any questions please contact us at medipoint72@mail.com.</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>`         
 };
 
 // e-mail transport configuration
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'kmimo7na@gmail.com',
-      pass: 'hjongizomfmjrlik'
+      user: 'medipoint72@gmail.com',
+      pass: 'ysmokjcjpkjblwil'
     }
 });
 
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const Doctor = db.doctors;
 const app = new Express();
 
 //static files use
-app.use(Express.static(path.join(__dirname, "public")));
+//app.use(Express.static(path.join(__dirname, "public")));
 
 //configuring dotenv
 dotenv.config();
@@ -194,7 +194,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //for using external JS file
-app.use("/JS",Express.static(__dirname + "/JS"));
+//app.use("/JS",Express.static(__dirname + "/JS"));
+app.use("/public/images",Express.static(__dirname + "/public/images"));
 
 //routing for home page
 const router = require('./routes/web');
@@ -227,38 +228,166 @@ const appointments = await PatientAppDetail.findAll({
       attributes: ['Full_Name']
     }]
   })
-  console.log(JSON.stringify(appointments));
-  console.log(tomorrow.toISOString().split("T")[0]);
-  console.log(JSON.stringify(doctor));
-  console.log(doctor.User.Full_Name);
-  console.log(appointments[0].Patients[0].User.Full_Name);
-  console.log(appointments[0].Patients[0].User.Email);
-  console.log(appointments[0].Patients[0].Patient_Appointments.Doctor_ID);
-  console.log(appointments[0].App_Date);
-  console.log(appointments[0].App_Time);
-  console.log(appointments[0].App_Type);
   let transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'kmimo7na@gmail.com',
-      pass: 'hjongizomfmjrlik'
+      user: 'medipoint72@gmail.com',
+      pass: 'ysmokjcjpkjblwil'
     }
   });
   
-  cron.schedule('00 00 13 * * *', () => {
+  cron.schedule('00 45 10 * * *', () => {
     const sendWishes =  
       // looping through the users
      appointments.forEach(appointment => {
         const mailOptions = {
-        from: 'kmimo7na@gmail.com',
+        from: {
+          name: 'MediPoint',
+          address: 'medipoint72@gmail.com'
+        },
         to: appointment.Patients[0].User.Email,
         subject: `Appointment reminder `,
-        html: `<b>Dear, ${appointment.Patients[0].User.Full_Name}</b> You have an appointment with Dr. ${doctor.User.Full_Name} tomorrow.\n <small>this is auto generated</small>`                       
+        html: ` 
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed;background-color:#f9f9f9" id="bodyTable">
+	<tbody>
+		<tr>
+			<td style="padding-right:10px;padding-left:10px;" align="center" valign="top" id="bodyCell">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperWebview" style="max-width:600px">
+					<tbody>
+						<tr>
+							<td align="center" valign="top">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%">
+									<tbody>
+										<tr>
+											<td style="padding-top: 20px; padding-bottom: 20px; padding-right: 0px;" align="right" valign="middle" class="webview"> <a href="#" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:right;text-decoration:underline;padding:0;margin:0" target="_blank" class="text hideOnMobile"></a>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperBody" style="max-width:600px">
+					<tbody>
+						<tr>
+							<td align="center" valign="top">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableCard" style="background-color:#fff;border-color:#e5e5e5;border-style:solid;border-width:0 1px 1px 1px;">
+									<tbody>
+										<tr>
+											<td style="background-color:#00d2f4;font-size:1px;line-height:3px" class="topBorder" height="3">&nbsp;</td>
+										</tr>
+										<tr>
+											<td style="padding-top: 60px; padding-bottom: 20px;" align="center" valign="middle" class="emailLogo">
+												<a href="#" style="text-decoration:none" target="_blank">
+													<img alt="" border="0" src="https://i.ibb.co/6mRFFzS/logo.png" style="width:100%;max-width:150px;height:auto;display:block" width="150">
+												</a>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
+												<h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Hi, ${appointment.Patients[0].User.Full_Name}</h2>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding-bottom: 30px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="subTitle">
+												<h4 class="text" style="color:#999;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:16px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:24px;text-transform:none;text-align:center;padding:0;margin:0">This is an appointment reminder for you</h4>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding-left:20px;padding-right:20px" align="center" valign="top" class="containtTable ui-sortable">
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableDescription" style="">
+													<tbody>
+														<tr>
+															<td style="padding-bottom: 20px;" align="center" valign="top" class="description">
+																<p class="text" style="color:#666;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:14px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:22px;text-transform:none;text-align:center;padding:0;margin:0">You have an medical appointment with <b>Dr. ${doctor.User.Full_Name}</b> tommorow at Tarakeshwor Health Clinic.</p>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableButton" style="">
+													<tbody>
+														<tr>
+															<td style="padding-top:20px;padding-bottom:20px" align="center" valign="top">
+																<table border="0" cellpadding="0" cellspacing="0" align="center">
+																	<tbody>
+																		<tr>
+																			<td style="background-color: rgb(0, 210, 244); padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton"> <a href="#" style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-decoration:none;display:block" target="_blank" class="text">Date: ${appointments[0].App_Date} <br> <br> Time: ${appointments[0].App_Time} <br> <br> Type: ${appointments[0].App_Type} appointment</a>
+																			</td>
+																		</tr>
+																	</tbody>
+																</table>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</td>
+										</tr>
+										<tr>
+											<td style="font-size:1px;line-height:1px" height="20">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" class="space">
+									<tbody>
+										<tr>
+											<td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperFooter" style="max-width:600px">
+					<tbody>
+						<tr>
+							<td align="center" valign="top">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" class="footer">
+									<tbody>
+										<tr>
+											<td style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px" align="center" valign="top" class="socialLinks">
+												<a href="#facebook-link" style="display:inline-block" target="_blank" class="facebook">
+													<img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/facebook.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+												</a>
+												<a href="#instagram-link" style="display: inline-block;" target="_blank" class="instagram">
+													<img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/instagram.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+												</a>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding: 10px 10px 5px;" align="center" valign="top" class="brandInfo">
+												<p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">©&nbsp;MediPoint Inc. | Tarakeshwor, 08 | Kathmandu, NEPAL.</p>
+											</td>
+										</tr>
+										<tr>
+											<td style="padding: 0px 10px 10px;" align="center" valign="top" class="footerEmailInfo">
+												<p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">If you have any questions please contact us at medipoint72@mail.com.</p>
+											</td>
+										</tr>
+										<tr>
+											<td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>`              
     };
     return transporter.sendMail(mailOptions, (error, data) => {
       if (error) {
           console.log(error)
           return
+      }else{
+        console.log(data.response);
       }
   });
     
